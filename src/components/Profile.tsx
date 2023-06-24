34fd99d7bb7cdfa2997c5e35c9cc2879f20dd3b0
hiromu-ikeda <hiromu.ikeda@ynot.jp>
2023-06-24 17:33:35 +0900

    スケルトンを追加

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import { useContext } from "react";
 import { LiffContext } from "./LiffProvider";
 
@@ -8,9 +9,17 @@ export const Profile = () => {
 
   const profile = liff?.getDecodedIDToken();
 
+  if (!profile?.picture)
+    return (
+      <div className="flex items-center gap-4">
+        <div className="w-8 h-8 bg-gray-300 rounded-[50%]" />
+        <div className="w-[72px] h-4 bg-gray-300" />
+      </div>
+    );
+
   return (
-    <div>
-      <button onClick={() => liff?.login()}>ログイン</button>
+    <div className="flex items-center gap-4">
+      <Image src={profile.picture} width={32} height={32} alt="プロフィール" />
       <p>名前 : {profile?.name}</p>
     </div>
   );
