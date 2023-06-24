750c6429cf925fad4d2ec821c198b394a1da9555
hiromu-ikeda <hiromu.ikeda@ynot.jp>
2023-06-24 17:18:03 +0900

    関数を修正

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,22 +1,17 @@
 "use client";
 
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import { LiffContext } from "./LiffProvider";
 
 export const Profile = () => {
   const liff = useContext(LiffContext);
-  const [name, setName] = useState<string | undefined>();
 
-  useEffect(() => {
-    (async () => {
-      const profile = await liff?.getDecodedIDToken();
-      setName(profile?.name);
-    })();
-  }, []);
+  const profile = liff?.getDecodedIDToken();
+
   return (
     <div>
       <button onClick={() => liff?.login()}>ログイン</button>
-      <p>名前 : {name}</p>
+      <p>名前 : {profile?.name}</p>
     </div>
   );
 };
