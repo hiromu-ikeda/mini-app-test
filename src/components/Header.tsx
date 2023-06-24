5b0c92e80309880c3adbf6233ec9324796103f02
hiromu-ikeda <hiromu.ikeda@ynot.jp>
2023-06-24 18:39:34 +0900

    スタイルを調整

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { UserProfile } from "./UserProfile";
 export const Header = () => {
   return (
     <header className="flex justify-between items-center px-4 py-2">
-      <h1>いきいき通販 ポータル</h1>
+      <h1 className="font-bold text-gray-700">いきいき通販 ポータル</h1>
       <UserProfile />
     </header>
   );
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,22 +1,19 @@
-import {
-  HomeIcon,
-  NewspaperIcon,
-  ShoppingCartIcon,
-} from "@heroicons/react/24/outline";
+import { NewspaperIcon, ShoppingCartIcon } from "@heroicons/react/24/outline";
+import { HomeIcon } from "@heroicons/react/24/solid";
 
 export const Navigation = () => {
   return (
-    <div className="flex justify-around">
+    <div className="flex justify-around py-2">
       <div className="flex flex-col items-center">
-        <HomeIcon className="w-8 h-8 text-gray-800" />
-        <p className="text-[12px]">ホーム</p>
+        <HomeIcon className="w-7 h-7 text-gray-700 p-1 rounded-[50%]" />
+        <p className="text-[12px] text-gray-700">ホーム</p>
       </div>
       <div className="flex flex-col items-center text-gray-300">
-        <NewspaperIcon className="w-8 h-8" />
+        <NewspaperIcon className="w-7 h-7 p-1" />
         <p className="text-[12px]">コラム</p>
       </div>
       <div className="flex flex-col items-center text-gray-300">
-        <ShoppingCartIcon className="w-8 h-8" />
+        <ShoppingCartIcon className="w-7 h-7 p-1" />
         <p className="text-[12px]">通販</p>
       </div>
     </div>
