70478ba3cfe2c3e48b343e15c9f850bc5f390012
hiromu-ikeda <hiromu.ikeda@ynot.jp>
2023-06-24 16:15:17 +0900

    liffを追加

---
+++ src/components/LiffProvider.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { Liff } from "@line/liff/exports";
+import { ReactNode, createContext, useEffect, useState } from "react";
+
+interface Props {
+  children: ReactNode;
+}
+
+const LiffContext = createContext<Liff | null>(null);
+
+export const LiffProvider = ({ children }: Props) => {
+  const [liffObject, setLiffObject] = useState<Liff | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      import("@line/liff")
+        .then((liff) => liff.default)
+        .then((liff) => {
+          liff
+            .init({ liffId: process.env.NEXT_PUBLIC_LIFF_ID! })
+            .then(() => {
+              setLiffObject(liff);
+            })
+            .catch((error: Error) => {
+              console.log(error);
+            });
+        });
+    })();
+  }, []);
+
+  return (
+    <LiffContext.Provider value={liffObject}>{children}</LiffContext.Provider>
+  );
+};
