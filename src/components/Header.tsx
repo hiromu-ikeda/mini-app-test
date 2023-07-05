44c36b565615de4eab5243d63e84992bb6408e84
hiromu-ikeda <hiromu.ikeda@ynot.jp>
2023-07-05 18:57:54 +0900

    ロゴを追加

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,13 @@
+import Image from "next/image"
 import { UserProfile } from "./UserProfile"
 
 export const Header = () => {
   return (
     <header className="flex justify-between items-center px-4 py-2 border-b border-gray-300">
-      <h1 className="font-bold text-gray-700">いきいき通販 ポータル</h1>
+      <div className="flex items-center gap-1">
+        <Image src="/logo.png" width={64} height={64} alt="ロゴ" />
+        <h1 className="font-bold text-gray-700">いきいき通販 ポータル</h1>
+      </div>
       <UserProfile />
     </header>
   )
